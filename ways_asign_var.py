23beabaa133bd7d11f5c8f2c937eb848cad17ffc
pranu1111 <107885176+pranu1111@users.noreply.github.com>
2022-06-23 01:52:39 +0530

    Add files via upload

---
+++ ways_asign_var.py
@@ -0,0 +1,18 @@
+a=10
+print(a)
+a,b,c=5,6,7
+print(a,b,c)
+print(a)
+# x,y,z=25,3.............ValueError: not enough values to unpack (expected 3, got 2)
+# print(x,y,z)
+#p,q,r,s=10,8,1,2,3..........ValueError: too many values to unpack (expected 4)
+#print(s)
+p,s,c=1,1,8.0
+print(p,s,c)
+print(p,s)
+p="Pranita"
+print(p)
+r,s="Rani","Soni"
+print(r,s,p)
+print(s)
+print(s.isidentifier())
